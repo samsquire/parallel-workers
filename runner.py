dade58dc7cfec7cc1aa5ad9aed7fb56891a7d4e8
Sam Squire <sam@samsquire.com>
2019-10-01 09:47:38 +0100

    Sam add missing layer of waiting

--- runner.py
+++ runner.py
@@ -18,33 +18,52 @@ work = [[['terraform/users/validate'],
   ['terraform/bastion/test']]]
 
 class Doer(Thread):
-    def __init__(self, group, doer_cache):
+    def __init__(self, item, doer_cache):
         super(Doer, self).__init__()
-        self.group = group
+        self.item = item
         self.doer_cache = doer_cache
 
     def run(self):
-        for item in self.group:
-            if item in self.doer_cache:
-                doer_cache[item].join()
-                print("Waiting for already started process {}".format(item))
-            else:
-                print("Running {}".format(item))
-                doer_cache[item] = self
+        print("Running {}".format(self.item))
+            
 
-class Worker(Thread):
+class Grouper(Thread):
     def __init__(self, run_groups, doer_cache):
-        super(Worker, self).__init__()   
+        super(Grouper, self).__init__()   
         self.run_groups = run_groups
         self.doer_cache = doer_cache
         
     def run(self):
         doers = []
         for group in self.run_groups: 
-            doer = Doer(group, doer_cache)
+            if group in doer_cache:
+                doer = doer_cache[group]
+                print("Waiting for existing running {}".format(group))
+                doer_cache[group].join()
+            else:
+                doer = Doer(group, doer_cache)
+                doer.start()
+                doer_cache[group] = doer
+            doers.append(doer)
+            
+        print("waiting for doers")
+        for doer in doers:
+            doer.join()
+
+
+class StreamWorker(Thread):
+    def __init__(self, run_groups, doer_cache):
+        super(StreamWorker, self).__init__()   
+        self.run_groups = run_groups
+        self.doer_cache = doer_cache
+        
+    def run(self):
+        doers = []
+        for group in self.run_groups: 
+            doer = Grouper(group, doer_cache)
             doers.append(doer)
             doer.start()
-        print("waiting for doers")
+        print("waiting for grouper")
         for doer in doers:
             doer.join()
 
@@ -53,7 +72,7 @@ stream_workers = []
 doer_cache = {}
 
 for stream in work:
-    stream_worker = Worker(stream, doer_cache)
+    stream_worker = StreamWorker(stream, doer_cache)
     stream_workers.append(stream_worker)
     stream_worker.start()
 
